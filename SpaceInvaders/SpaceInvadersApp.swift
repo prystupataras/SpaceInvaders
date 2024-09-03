5fcf146449db30415b32c57758b97c5d95f89b41
Taras <45391115+prystupataras@users.noreply.github.com>
2024-09-03 09:52:45 +0300

    Initial Commit

---
+++ SpaceInvaders/SpaceInvadersApp.swift
@@ -0,0 +1,17 @@
+//
+//  SpaceInvadersApp.swift
+//  SpaceInvaders
+//
+//  Created by Prystupa Taras on 03.09.2024.
+//
+
+import SwiftUI
+
+@main
+struct SpaceInvadersApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
